eb3771bb17f1a77745ee415255f75f271f9fcb7f
Amir Afshar <mr.afshar0@gmail.com>
2024-09-13 19:01:34 +0330

    Add missing import

--- src/commands/module.ts
+++ src/commands/module.ts
@@ -1,33 +1,34 @@
-import {consola} from "consola";
-import {defineCommand} from "../index";
-import {modulesList} from "../models/modulesList";
+import { consola } from "consola";
+import { defineCommand } from "../index";
+import { modulesList } from "../models/modulesList";
+import { getTemplateWithGiget } from "../utils/giget";
 
 export default defineCommand({
-    meta: {
-        name: "addModule",
-        description: "global utility management for frontend",
+  meta: {
+    name: "addModule",
+    description: "global utility management for frontend",
+  },
+  args: {
+    template: {
+      type: "positional",
+      description: "module templates",
+      default: "addModule",
+      required: false,
     },
-    args: {
-        template: {
-            type: "positional",
-            description: "module templates",
-            default: "addModule",
-            required: false,
-        },
-    },
-    async run(ctx) {
-        try {
-            const selectedModuleName = await consola.prompt(
-                "Select your desired module to add",
-                {
-                    type: "select",
-                    options: modulesList,
-                }
-            );
-            // @ts-ignore
-            await getTemplateWithGiget(selectedModuleName);
-        } catch (e) {
-            consola.error(e);
+  },
+  async run(ctx) {
+    try {
+      const selectedModuleName = await consola.prompt(
+        "Select your desired module to add",
+        {
+          type: "select",
+          options: modulesList,
         }
-    },
+      );
+      // @ts-ignore
+      await getTemplateWithGiget(selectedModuleName);
+    } catch (e) {
+      consola.error(e);
+    }
+  },
 });
--- src/utils/giget.ts
+++ src/utils/giget.ts
@@ -1,108 +1,107 @@
-import {exec} from "child_process";
+import { exec } from "child_process";
 import consola from "consola";
 import {
-    checkAndInstallPackages,
-    getConfig,
-    getUserCurrentPackageManagerFromPrompt,
-    IInstallPackagesCommandResult,
+  checkAndInstallPackages,
+  getConfig,
+  getUserCurrentPackageManagerFromPrompt,
+  IInstallPackagesCommandResult,
 } from "../../src/internals/utilities";
-import {IPackageManager, packageManagers} from "../models/packageManagers";
-import {IModule, moduleTemplates} from "./moduleTemplates";
-import {createFile, getContent} from "./fs";
-import {pwaConfig} from "./templates";
-import {downloadTemplate} from "giget";
+import { IPackageManager, packageManagers } from "../models/packageManagers";
+import { IModule, moduleTemplates } from "./moduleTemplates";
+import { createFile, getContent } from "./fs";
+import { pwaConfig } from "./templates";
+import { downloadTemplate } from "giget";
 
 let selectedPackageManager: IPackageManager = {};
 let dependenciesCommand: IInstallPackagesCommandResult | null = null;
 
 async function getTemplateWithGiget(template: string) {
-    // Get Package Manager Name from user input
-    const selectedPackageManagerName =
-        await getUserCurrentPackageManagerFromPrompt();
-    // @ts-ignore
-    selectedPackageManager = packageManagers[selectedPackageManagerName];
+  // Get Package Manager Name from user input
+  const selectedPackageManagerName =
+    await getUserCurrentPackageManagerFromPrompt();
+  // @ts-ignore
+  selectedPackageManager = packageManagers[selectedPackageManagerName];
 
-    // Load the config of module that user selected
-    const selectedModuleTemplateConfig = moduleTemplates[template] as IModule;
-    try {
-        // Save current gitignore of user project
-        const originalGitIgnore = await getOriginalContent('.gitignore');
-        const originalReadme = await getOriginalContent('readme.MD');
+  // Load the config of module that user selected
+  const selectedModuleTemplateConfig = moduleTemplates[template] as IModule;
+  try {
+    // Save current gitignore of user project
+    const originalGitIgnore = await getOriginalContent(".gitignore");
+    const originalReadme = await getOriginalContent("readme.MD");
 
-        // Download selected module from github
-        await downloadTemplate(selectedModuleTemplateConfig.url, {
-            cwd: process.cwd(),
-            dir: "./",
-            force: true,
-        });
+    // Download selected module from github
+    await downloadTemplate(selectedModuleTemplateConfig.url, {
+      cwd: process.cwd(),
+      dir: "./",
+      force: true,
+    });
 
-        // If there was a gitignore before downloading, rewrite it back
-        if (originalGitIgnore) {
-            await restoreContent(originalGitIgnore, '.gitignore');
-        }
-        if (originalReadme) {
-            await restoreContent(originalReadme, '.readme.MD');
-        }
-
-        dependenciesCommand = await checkAndInstallPackages(
-            selectedModuleTemplateConfig
-        );
-        if (dependenciesCommand.dep || dependenciesCommand.devDep) {
-            await runInstallCommand();
-        }
-        if (template === 'pwa') {
-            await addPWAToNuxt()
-        }
-    } catch (e) {
-        consola.error(e);
+    // If there was a gitignore before downloading, rewrite it back
+    if (originalGitIgnore) {
+      await restoreContent(originalGitIgnore, ".gitignore");
     }
+    if (originalReadme) {
+      await restoreContent(originalReadme, ".readme.MD");
+    }
+
+    dependenciesCommand = await checkAndInstallPackages(
+      selectedModuleTemplateConfig
+    );
+    if (dependenciesCommand.dep || dependenciesCommand.devDep) {
+      await runInstallCommand();
+    }
+    if (template === "pwa") {
+      await addPWAToNuxt();
+    }
+  } catch (e) {
+    consola.error(e);
+  }
 }
 
 async function addPWAToNuxt() {
-    const applicationName = await consola.prompt("Enter Your Application Name");
-    const themeColor = await consola.prompt("Enter A Primary Color (Exp: #ccc)");
-    // @ts-ignore
-    pwaConfig.manifest.short_name = applicationName
-    // @ts-ignore
-    pwaConfig.manifest.name = applicationName
-    // @ts-ignore
-    pwaConfig.manifest.theme_color = themeColor
-    // @ts-ignore
-    pwaConfig.manifest.background_color = themeColor
-    await getConfig('nuxt.config', (nuxtConfig) => {
-        nuxtConfig['pwa'] = pwaConfig
-    })
+  const applicationName = await consola.prompt("Enter Your Application Name");
+  const themeColor = await consola.prompt("Enter A Primary Color (Exp: #ccc)");
+  // @ts-ignore
+  pwaConfig.manifest.short_name = applicationName;
+  // @ts-ignore
+  pwaConfig.manifest.name = applicationName;
+  // @ts-ignore
+  pwaConfig.manifest.theme_color = themeColor;
+  // @ts-ignore
+  pwaConfig.manifest.background_color = themeColor;
+  await getConfig("nuxt.config", (nuxtConfig) => {
+    nuxtConfig["pwa"] = pwaConfig;
+  });
 }
 
 async function getOriginalContent(fileName: string) {
-    return await getContent(`${process.cwd()}/${fileName}`);
+  return await getContent(`${process.cwd()}/${fileName}`);
 }
 
-
 async function restoreContent(
-    content: string | NodeJS.ArrayBufferView,
-    fileName: string
+  content: string | NodeJS.ArrayBufferView,
+  fileName: string
 ) {
-    await createFile({
-        directoryPath: process.cwd(),
-        fileContent: content,
-        fileName,
-    });
+  await createFile({
+    directoryPath: process.cwd(),
+    fileContent: content,
+    fileName,
+  });
 }
 
 async function runInstallCommand() {
-    await exec(
-        `${selectedPackageManager.command} ${dependenciesCommand?.dep} & ${selectedPackageManager.command} ${selectedPackageManager.dev} ${dependenciesCommand?.devDep}`,
-        (error, stdout, stderr) => {
-            if (error) {
-                consola.error("error happened:", error);
-            }
-            if (stderr) {
-                consola.error("std happened: ", stderr);
-            }
-            consola.success("stdout", stdout);
-        }
-    );
+  await exec(
+    `${selectedPackageManager.command} ${dependenciesCommand?.dep} & ${selectedPackageManager.command} ${selectedPackageManager.dev} ${dependenciesCommand?.devDep}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        consola.error("error happened:", error);
+      }
+      if (stderr) {
+        consola.error("std happened: ", stderr);
+      }
+      consola.success("stdout", stdout);
+    }
+  );
 }
 
-export {getTemplateWithGiget};
+export { getTemplateWithGiget };
