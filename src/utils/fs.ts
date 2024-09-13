c6b50794db45809bd4406fbb2e119ade28c25c68
saman <samanft752@gmail.com>
2024-09-13 13:59:31 +0330

    added encoding option to getContent function

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,36 +1,37 @@
 import fs from "fs";
 
 interface IOpts {
-  directoryPath: string;
-  fileName?: string;
-  fileContent: string | NodeJS.ArrayBufferView;
+    directoryPath: string;
+    fileName?: string;
+    fileContent: string | NodeJS.ArrayBufferView;
 }
 
 // Check if a file exists
 export async function exists(path: string) {
-  try {
-    await fs.promises.access(path);
-    return true;
-  } catch {
-    return false;
-  }
+    try {
+        await fs.promises.access(path);
+        return true;
+    } catch {
+        return false;
+    }
 }
 
 async function createFile(opts: IOpts) {
-  const isDirectoryExists = await exists(opts.directoryPath);
-  if (!isDirectoryExists) {
-    fs.mkdirSync(opts.directoryPath);
-  }
-  fs.writeFileSync(`${opts.directoryPath}/${opts.fileName}`, opts.fileContent);
+    const isDirectoryExists = await exists(opts.directoryPath);
+    if (!isDirectoryExists) {
+        fs.mkdirSync(opts.directoryPath);
+    }
+    fs.writeFileSync(`${opts.directoryPath}/${opts.fileName}`, opts.fileContent);
 }
 
-async function getContent(filePath: string) {
-  const isFileExists = await exists(filePath);
-  if (isFileExists) {
-    return fs.readFileSync(filePath);
-  } else {
-    throw "no such file !";
-  }
+async function getContent(filePath: string, encoding?: string) {
+    const isFileExists = await exists(filePath);
+    if (isFileExists) {
+        // @ts-ignore
+        return fs.readFileSync(filePath, {encoding});
+    } else {
+        throw "no such file !";
+    }
 }
 
-export { createFile, getContent };
+export {createFile, getContent};
